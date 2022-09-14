ac788f9448a0a9c32cfcc9ccaff90279d3faf8dc
Ron Kosova <ron.kosova@gmail.com>
2022-09-14 17:50:46 +0300

    Changed secret key to 32 bit hex token

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '2115'
+app.config['SECRET_KEY'] = '7c7b7cd8dacf674501f743dfa8c57d90'
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
